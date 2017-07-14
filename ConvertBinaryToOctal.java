21b032e81f72db1a2f3ed00ba5739e75c2e2a568
Neel <neel@meta.com>
2017-07-14 17:18:59 +0530

    All Assignments

--- ConvertBinaryToOctal.java
+++ ConvertBinaryToOctal.java
@@ -12,44 +12,50 @@ public class ConvertBinaryToOctal {
 	 */
 
 	public int convertBinaryToOctal(int n) {
-		
+
 		int triad; // This variable stores the pair of 3 digits of given binary number.
 		int flag = 0; // This variable contains octal value of the triad.
 		int octal = 0; // This variable is the final octal value of the binary number.
-		int count = 1; // This variable is used for placing the octal value at unit's place, ten's place and so on.
-		
-		while (n > 0){
+		int count = 1; // This variable places the octal value at unit's place, ten's place and so on.
+		/*
+		 * This loop goes on until all the possible triads are converted
+		 * to their respective octal values. 
+		 */
+		while (n > 0) {
 			triad = n % 1000;
 			n /= 1000;
-			
-			switch (triad){
+
+			//These statements convert all the triads to their respective octal values.
+			switch (triad) {
 			case 0: flag = 0;
-				break;
+			break;
 			case 1: flag = 1;
-				break;
+			break;
 			case 10: flag = 2;
-				break;
+			break;
 			case 11: flag = 3;
-				break;
+			break;
 			case 100: flag = 4;
-				break;
+			break;
 			case 101: flag = 5;
-				break;
+			break;
 			case 110: flag = 6;
-				break;
+			break;
 			case 111: flag = 7;
-				break;
-			default: flag = 8;
-				break;
+			break;
+			default: flag = 8; // This flag used if number is not binary.
+			break;
 			}
-			if(flag!=8){
+			// It runs only if number is a binary number.
+			if (flag != 8) {
 				octal += flag * count;
 				count *= 10;
 			}
+			// It runs if number is not a binary number.
 			else{
-				octal = 8;
+				octal = 8; // 8 is used because it can never be in octal number.
 			}
 		}
 		return octal;
-}
+	}
 }
--- ConvertBinaryToOctalTest.java
+++ ConvertBinaryToOctalTest.java
@@ -4,27 +4,29 @@ import org.junit.Test;
 public class ConvertBinaryToOctalTest {
 
 	@Test
-	 public void evaluatesExpression1() {
-	   ConvertBinaryToOctal binToOct = new ConvertBinaryToOctal();
-	   int octal = binToOct.convertBinaryToOctal(1010010);
-	   assertEquals(122, octal);
-	 }
-	
+	public void evaluatesExpression1() {
+		ConvertBinaryToOctal binToOct = new ConvertBinaryToOctal();
+		int octal = binToOct.convertBinaryToOctal(1010010);
+		assertEquals(122, octal);
+	}
+
 	@Test
-	 public void evaluatesExpression2() {
-	   ConvertBinaryToOctal binToOct = new ConvertBinaryToOctal();
-	   int octal = binToOct.convertBinaryToOctal(11010010);
-	   assertEquals(322, octal);
-	 }
-	
+	public void evaluatesExpression2() {
+		ConvertBinaryToOctal binToOct = new ConvertBinaryToOctal();
+		int octal = binToOct.convertBinaryToOctal(11010010);
+		assertEquals(322, octal);
+	}
+
+	// This is a special case when a non binary number is given as input.
+
 	@Test
-	 public void evaluatesExpression3() {
-	   ConvertBinaryToOctal binToOct = new ConvertBinaryToOctal();
-	   int octal = binToOct.convertBinaryToOctal(12340000);
-	   String errorMessage = null;
-	   if (octal == 8){
-		   errorMessage = "Please insert a binary value!!!";
-	   }
-	   assertEquals(errorMessage, "Please insert a binary value!!!");
-	 }
+	public void evaluatesExpression3() {
+		ConvertBinaryToOctal binToOct = new ConvertBinaryToOctal();
+		int octal = binToOct.convertBinaryToOctal(12340000);
+		String errorMessage = null;
+		if (octal == 8){
+			errorMessage = "Please insert a binary value!!!";
+		}
+		assertEquals(errorMessage, "Please insert a binary value!!!");
+	}
 }
