611380bce26d4d5f2c954eda2934c35924bf58cb
meta-neel-singhal <neelkumar.singhal@metacube.com>
2017-08-08 01:09:55 +0530

    Javascript-1 Assignments

--- JavaScript-1 Assignments/js/script.js
+++ JavaScript-1 Assignments/js/script.js
@@ -6,62 +6,62 @@ function Home() {
 }
 
 function validateName(){
-	var x;
-	if ((x = document.forms["contact-us"]["username"].value) == "" || !x.match( /^[a-zA-z]+$/)){
-		document.forms["contact-us"]["username"].style.border = "2px solid red";
+	var data;
+	if ((data = document.forms["contact-us"]["username"].value) == "" || !data.match( /^[a-zA-z]+$/)){
+		document.forms["contact-us"]["username"].style.border = "2px solid #f00";
 		return false;
 	} else{
-        document.forms["contact-us"]["username"].style.border = "2px solid green";
+        document.forms["contact-us"]["username"].style.border = "2px solid #0f0";
         return true;
 	}
 }
 
 function validateEmail(){
-	var x;	
-	if((x = document.forms["contact-us"]["useremail"].value) == "" || !x.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/)){
-		document.forms["contact-us"]["useremail"].style.border = "2px solid red";
+	var data;	
+	if((data = document.forms["contact-us"]["useremail"].value) == "" || !data.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/)){
+		document.forms["contact-us"]["useremail"].style.border = "2px solid #f00";
 		return false;
 	} else{
-	    document.forms["contact-us"]["useremail"].style.border = "2px solid green";
+	    document.forms["contact-us"]["useremail"].style.border = "2px solid #0f0";
 	    return true;
 	}
 }
 
 function validateOrganization(){
-	var x;
-	if((x = document.forms["contact-us"]["userorg"].value) == "" || !x.match(/^[a-zA-z]+$/)){
-	    document.forms["contact-us"]["userorg"].style.border = "2px solid red";
+	var data;
+	if((data = document.forms["contact-us"]["userorg"].value) == "" || !data.match(/^[a-zA-z]+$/)){
+	    document.forms["contact-us"]["userorg"].style.border = "2px solid #f00";
 		return false;
 	} else{
-		document.forms["contact-us"]["userorg"].style.border = "2px solid green";
+		document.forms["contact-us"]["userorg"].style.border = "2px solid #0f0";
 	    return true;
 	}		
 }
 
 function validatePhoneNumber(){
-	var x;
-    if((x = document.forms["contact-us"]["usercontact"].value) == "" || !x.match(/^[0-9]{6,12}$/)){
-        document.forms["contact-us"]["usercontact"].style.border = "2px solid red";
+	var data;
+    if((data = document.forms["contact-us"]["usercontact"].value) == "" || !data.match(/^[0-9]{6,12}$/)){
+        document.forms["contact-us"]["usercontact"].style.border = "2px solid #f00";
         return false;
 	} else{
-		document.forms["contact-us"]["usercontact"].style.border = "2px solid green";
+		document.forms["contact-us"]["usercontact"].style.border = "2px solid #0f0";
 	    return true;
 	}	
 }
 
 function validateMessage(){
-
-    if(document.getElementById("usermessage").value == "" || document.getElementById("message").value.length>250){
-        document.getElementById("usermessage").style.border = "2px solid red";
+	var data;
+    if((data = document.forms["contact-us"]["usermessage"].value) == "" || data.length > 250){
+        document.forms["contact-us"]["usermessage"].style.border = "2px solid #f00";
         return false;
 	} else{
-		document.getElementById("usermessage").style.border = "2px solid green";
+		document.forms["contact-us"]["usermessage"].style.border = "2px solid #0f0";
 	    return true;
-	}	
+	}
 }
 
 function validateForm(){
-    if(!validateName() || !validateEmail() || !validateOrganization() || !validateMessage() || !validatePhoneNumber()){
+    if(!validateName() || !validateEmail() || !validateOrganization() || !validateMessage() || !validatePhoneNumber() || !cityOption() ){
         validateName();
         validateEmail();
         validateOrganization();
@@ -79,9 +79,12 @@ function cityOption(){
 	var selectedValue = element.options[element.selectedIndex].value;
 
 	if(selectedValue == "default"){
-		document.getElementById("usercity").style.border = "2px solid red";
+		document.getElementById("usercity").style.border = "2px solid #f00";
 		document.getElementById("selectedcity").value = "No option selected";
+		return false;
 	} else{
-	    document.getElementById("selectedcity").value = selectedValue;
+		document.getElementById("usercity").style.border = "2px solid #0f0";
+		document.getElementById("selectedcity").value = selectedValue;
+		return true;
 	}
 }
