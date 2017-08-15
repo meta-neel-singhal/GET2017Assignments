f06e4e806076159386889f5dc8d450e85dd175f1
meta-neel-singhal <neelkumar.singhal@metacube.com>
2017-08-15 17:57:17 +0530

    JavaScript session - 3 Assignments

--- JavaScript-3 Assignments/Question3 - Web Worker/js/script.js
+++ JavaScript-3 Assignments/Question3 - Web Worker/js/script.js
@@ -7,12 +7,14 @@ var webWorker = {}; // Global variable declaration.
  * Set up eventListeners when window is loaded.
  */
 window.onload = function() {
-    var date = new Date();
-    alert(date);
+    showDate();
 
-	document.getElementById('start').addEventListener('click',startWorker);
-    document.getElementById('stop').addEventListener('click',stopWorker);
-    document.getElementById('checkAgain').addEventListener('click',checkAgain);
+	document.getElementById('start').addEventListener('click', startWorker);
+    document.getElementById('stop').addEventListener('click', stopWorker);
+    document.getElementById('reset').addEventListener('click', reset);
+    document.getElementById('hold').addEventListener('click', hold);
+    document.getElementById('showDate').addEventListener('click', showDate);
+    document.getElementById('printDate').addEventListener('click', showDate);
 };
 
 /**
@@ -45,7 +47,49 @@ function stopWorker() {
 /**
  * Resets the web worker to use it again.
  */
-function checkAgain() {
+function reset() {
     document.getElementById('result').innerHTML = 'Your Result Will Appear Here...';
     webWorker = {};
+}
+
+/**
+ * Used to set up the tabs. Displays the active tab and its corresponding data.
+ * 
+ * @param {*} event 
+ * @param {*} webWorkerPresence 
+ */
+function openWebWorker(event, webWorkerPresence) {
+    var count, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('cover');
+    // To hide the contents of the tab which is not selected.
+    for (count = 0; count < tabcontent.length; count++) {
+        tabcontent[count].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    // To change the status of active tab to none. 
+    for (count = 0; count < tablinks.length; count++) {
+        tablinks[count].className = tablinks[count].className.replace(' active', '');
+    }
+    // To display the contents of the selected tab.
+    document.getElementById(webWorkerPresence).style.display = 'block';
+    // To change the status of the selected tab to active.
+    event.currentTarget.className += ' active';
+}
+
+/**
+ * Hold the execution for two minutes (without the use of web worker).
+ */
+function hold() {
+    show.innerHTML = 'Message Box';
+    setTimeout(function () {
+        show.innerHTML = 'Hold for 2 minutes is over.';
+    }, 120000);
+}
+
+/**
+ * Displays the current date and time in the browser.
+ */
+function showDate() {
+    var date = new Date();
+    alert(date);
 }
