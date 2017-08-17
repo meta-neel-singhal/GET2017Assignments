8eb95e4675153415f23549d140200b7088079976
meta-neel-singhal <neelkumar.singhal@metacube.com>
2017-08-17 11:52:54 +0530

    Update WebServiceClient.java

--- Web Service - Assignment 1/WebServiceClient/src/webservice/WebServiceClient.java
+++ Web Service - Assignment 1/WebServiceClient/src/webservice/WebServiceClient.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * @author Neel Singhal
  */
 public class WebServiceClient {
-	
+
 	/**
 	 * The main method.
 	 *
@@ -17,38 +17,48 @@ public class WebServiceClient {
 	public static void main(String args[]) {
 		FahrenheitToCelsiusServiceLocator locatorObject = new FahrenheitToCelsiusServiceLocator();
 		locatorObject.setFahrenheitToCelsiusEndpointAddress("http://localhost:8080/Web_Service_Assignment_1/services/FahrenheitToCelsius");
-		boolean flag;
 		Scanner scan = new Scanner(System.in);;
 		double temperature;
-		String repeat;
-		// Run until user wants to convert temperature from fahrenheit to celsius.
-		do {
-			flag = false;
-			try {
-				FahrenheitToCelsius temperatureObject = locatorObject.getFahrenheitToCelsius();
-				System.out.println("Enter temperature in Fahrenheit: ");
-				temperature = scan.nextDouble();
-				System.out.println("Temperature in celsius is: " + temperatureObject.convertFahrenheitToCelsius(temperature));
-				System.out.println("Check another temperature: (y/n)");
-				repeat = scan.next();
-				// Check if user wants to convert another temperature from fahrenheit to celsius.
-				switch (repeat) {
-				case "y":
-				case "Y":
-					main(args);				
-					break;
-				default:
-					System.out.println("Thank You.");
-					System.exit(0);
-					break;
-				}
-			}
-			catch (Exception exception) {
-				System.out.println(exception.getMessage());
-			}
-			finally {
-				scan.close();
-			}
-		} while (flag);
+
+		try{
+			FahrenheitToCelsius temperatureObject = locatorObject.getFahrenheitToCelsius();
+			System.out.println("Enter temperature in Fahrenheit: ");
+			temperature = scan.nextDouble();
+			System.out.println("Temperature in celsius is: " + temperatureObject.convertFahrenheitToCelsius(temperature));
+			System.out.println("Check another temperature (y/n): ");
+			// Calling repeat function to check if user wish to continue.
+			repeat(scan);
+		}
+		catch (Exception exception) {
+			System.out.println("Please enter correct value!!!");
+			main(args);
+		}
+		finally {
+			scan.close();
+		}
+	}
+
+	/**
+	 * Check if user wants to convert another temperature from fahrenheit to celsius.
+	 *
+	 * @param scan    Scanner class object.
+	 */
+	public static void repeat(Scanner scan) {
+		String ans = scan.next();
+		switch (ans) {
+			case "y":
+			case "Y":
+				WebServiceClient.main(null);				
+				break;
+			case "n":
+			case "N":
+				System.out.println("Thank You.");
+				System.exit(0);				
+				break;
+			default:
+				System.out.println("Please enter correct choice (y/n): ");
+				repeat(scan);
+				break;
+		}
 	}
 }
