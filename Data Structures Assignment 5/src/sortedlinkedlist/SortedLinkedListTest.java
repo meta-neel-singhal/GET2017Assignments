f435819ce1006e5b4c5f9df7da6685a8a453e969
meta-neel-singhal <neelkumar.singhal@metacube.com>
2017-08-28 17:17:02 +0530

    Update SortedLinkedListTest.java

--- Data Structures Assignment 5/src/sortedlinkedlist/SortedLinkedListTest.java
+++ Data Structures Assignment 5/src/sortedlinkedlist/SortedLinkedListTest.java
@@ -1,10 +1,10 @@
 package sortedlinkedlist;
 
-import static org.junit.Assert.*;
-import org.junit.runners.MethodSorters;
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 /**
  * The Class to test Sorted Linked List.
@@ -12,62 +12,29 @@ import org.junit.Test;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SortedLinkedListTest {
 	
-	SortedList list;
-	
-	@Before
-	public  void init() {
-		list = new SortedList();
-	}
-	
 	// Positive test case.
 	@Test
 	public void sortingTest1() {
-		list.add(12);
-		list.add(6);
-		list.add(8);
-		list.add(16);
+		SortedList.add(12);
+		SortedList.add(6);
+		SortedList.add(8);
+		SortedList.add(16);
 		SortedList.add(3);
 				
-		String expected = list.sortedList.toString();
+		String expected = SortedList.sortedList.toString();
 		String actual = "[3, 6, 8, 12, 16]";
 		assertEquals(actual,expected);
 	} 
 
-	// Positive test case.
+	// Negative test case.
 	@Test
 	public void sortingTest2() {
-		list.add(12);
-		list.add(24);
-		list.add(1);
-		list.add(2);
-		list.add(30);
-		String expected = list.sortedList.toString();
-		String actual = "[1, 2, 12, 24, 30]";
-		assertEquals(actual,expected);
-	} 
-
-	// Negative test case.
-	@Test
-	public void sortingTest3() {
-		list.add(12);
-		list.add(6);
-		list.add(8);
-		list.add(16);
-		list.add(3);
-		String expected = list.sortedList.toString();
-		String actual = "[3, 8, 6, 12, 16]";
-		assertFalse(expected == actual);
-	} 
-
-	// Negative test case.
-	@Test
-	public void sortingTest4() {
-		list.add(12);
-		list.add(24);
-		list.add(1);
-		list.add(2);
-		list.add(30);
-		String expected = list.sortedList.toString();
+		SortedList.add(12);
+		SortedList.add(24);
+		SortedList.add(1);
+		SortedList.add(2);
+		SortedList.add(30);
+		String expected = SortedList.sortedList.toString();
 		String actual = "[1, 12, 2, 24, 30]";
 		assertFalse(expected == actual);
 	}
