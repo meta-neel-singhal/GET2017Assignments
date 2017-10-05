28ff34a943a4c83398cfc691e4c06ba4ed498697
Neel <neel@meta.com>
2017-10-05 19:12:31 +0530

    LA Assignment-4

---
+++ Layered Architecture Assignment - 4/frontend/src/app/product/product-update/product-update.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Observable } from 'rxjs/Observable';
+import { Product } from '../../model/product';
+import { ProductService } from '../../services/product.service';
+
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+    selector: 'app-product-update',
+    templateUrl: './product-update.component.html',
+    styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent {
+    product: Product;
+
+    constructor(
+        private productService: ProductService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) { }
+
+    ngOnInit(): void {
+        this.route.paramMap
+            .switchMap((params: ParamMap) => this.productService.getProduct(+ params.get('id')))
+            .subscribe(product => this.product = product);
+    }
+
+    save(): void {
+        this.productService.update(this.product)
+            .then(() => this.goBack());
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+}
